cc29b03ead9bfccbb5dabf48e65ba3618f8dd5a8
OmerKappon <58888293+OmerKappon@users.noreply.github.com>
2021-07-25 23:02:55 +0300

    Code cleanup
    posts component deleted

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import {DefaultComponent} from "./default.component";
 import {DashboardComponent} from "../../modules/dashboard/dashboard.component";
 import {RouterModule} from "@angular/router";
-// import {PostsComponent} from "../../modules/posts/posts.component";
 import {SharedModule} from "../../shared/shared.module";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatDividerModule} from "@angular/material/divider";
